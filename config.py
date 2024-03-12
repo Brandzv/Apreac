5efbf9ceb78973c8580595202122ec4badc46840
Bazv <brandonv.zamora@gmail.com>
2024-03-12 16:04:57 -0600

    Se añadió archivo con el esquema de colores

---
+++ config.py
@@ -0,0 +1,7 @@
+"""Module providing a function printing python version."""
+
+LEFTBAR_COLOR = "#363636"
+BODY_COLOR = "#FFFFFF"
+TEXT_COLOR = "#FFFFFF"
+HOVER_COLOR = "#FFB500"
+# HOVER_COLOR = "#08001D"
