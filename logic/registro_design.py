9cee8149073def51696d6d616054cc7b1b32be59
Bazv <brandonv.zamora@gmail.com>
2024-04-05 09:51:50 -0600

    Añadió variable "body_table"

--- logic/registro_design.py
+++ logic/registro_design.py
@@ -9,14 +9,15 @@ class RegisterDesign:
     """Clase del diseño del panel escaneo"""
 
     def __init__(self, body):
+        body_table = body
         frame = tk.LabelFrame(body, text="Registro",
                               font=("Helvetica", 16, "bold"))
         frame.config(bd=2)
         frame.pack(fill="x", padx=100, pady=2, ipady=5)
 
-        self.register_id(frame)
+        self.register_id(frame, body_table)
 
-    def register_id(self, frame):
+    def register_id(self, frame, body_table):
         """Función que crea el entry de ID"""
 
         self.label_id = tk.Label(
@@ -25,7 +26,7 @@ class RegisterDesign:
         self.entry_id = ttk.Entry(frame, width=18, font=("Helvetica", 13))
 
         # Función para verificar si el entry contiene números válidos
-        def validar_entry(search_id):
+        def validar_entry(search_id, body_table):
             try:
                 float(search_id)
                 return True
@@ -34,8 +35,8 @@ class RegisterDesign:
 
         def clic_button():
             search_id = self.entry_id.get()  # Obtiene el id del Entry
-            if validar_entry(search_id):
-                self.open_info_frame(frame, search_id)
+            if validar_entry(search_id, body_table):
+                self.open_info_frame(frame, search_id, body_table)
             else:
                 print("Por favor, ingresa números válidos en el Entry.")
 
@@ -56,10 +57,10 @@ class RegisterDesign:
         self.entry_id.focus_set()
         self.button_id.pack(padx=left_padding, pady=5)
 
-    def open_info_frame(self, frame, search_id):
+    def open_info_frame(self, frame, search_id, body_table):
         """Función que abre frame de información y cierra el panel de registro."""
         self.clear_panel(frame)
-        InfoDesign(frame, search_id)
+        InfoDesign(frame, search_id, body_table)
 
     def clear_panel(self, panel):
         """Función que se encarga de limpiar el contenido del frame"""
