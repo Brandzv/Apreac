3ed5a61bc399172151a86e653300404ca04cbf0c
Bazv <brandonv.zamora@gmail.com>
2024-04-05 22:39:55 -0600

    Se creo variable "body_table" y tabla bitácora

--- logic/registro_info_design.py
+++ logic/registro_info_design.py
@@ -7,7 +7,7 @@ from conexion import cursor
 class InfoDesign():
     """Clase del diseño del panel de registro de información"""
 
-    def __init__(self, frame, search_id):
+    def __init__(self, frame, search_id, body_table):
         # Cambio de tamaño del frame
         frame.pack(fill="x", padx=70, pady=2, ipady=5)
 
@@ -17,15 +17,16 @@ class InfoDesign():
 
         if alumno:
             self.id_alumno, self.nombre, self.apellido_paterno, self.apellido_materno, self.programa, self.rol = alumno
-            self.info_alumno(frame)
+            self.student_info(frame)
+            self.table_info(body_table)
 
         else:
             self.lbl = tk.Label(
                 frame, text="Estudiante no encontrado", font=("Helvetica", 13))
             self.lbl.pack()
 
-    def info_alumno(self, frame):
-        """A dummy description."""
+    def student_info(self, frame):
+        """Función mostrar información de alumno"""
 
         # Mostrar ID de alumno
         self.text_id = tk.Label(
@@ -93,3 +94,31 @@ class InfoDesign():
         self.text_rol.grid(row=3, column=2, pady=5, padx=5, sticky="w")
         self.show_rol.grid(row=3, column=3, pady=5, padx=5, sticky="ew")
         frame.columnconfigure(3, weight=1)
+
+    def table_info(self, body_table):
+        """Función tabla de registro bitácora"""
+        self.tree = ttk.Treeview(body_table, columns=(
+            "no", "pc", "fecha", "nombre", "rol", "programa", "hr_entrada", "hr_salida", "actividad"), show="headings")
+        self.tree.heading("no", text="No.")
+        self.tree.heading("pc", text="PC")
+        self.tree.heading("fecha", text="Fecha")
+        self.tree.heading("nombre", text="Nombre")
+        self.tree.heading("rol", text="Rol")
+        self.tree.heading("programa", text="Programa")
+        self.tree.heading("hr_entrada", text="Hora Entrada")
+        self.tree.heading("hr_salida", text="Hora Salida")
+        self.tree.heading("actividad", text="Actividad")
+
+        self.tree.column("no", anchor="w")
+        self.tree.column("pc", anchor="w")
+        self.tree.column("fecha", anchor="w")
+        self.tree.column("nombre", anchor="w")
+        self.tree.column("rol", anchor="w")
+        self.tree.column("programa", anchor="w")
+        self.tree.column("hr_entrada", anchor="w")
+        self.tree.column("hr_salida", anchor="w")
+        self.tree.column("actividad", anchor="w")
+
+        for column in self.tree['columns']:
+            self.tree.column(column, width=50,  anchor="center")
+        self.tree.pack(fill="both", expand=True, pady=1, padx=5)
