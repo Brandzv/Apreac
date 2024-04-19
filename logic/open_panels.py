b7ab9631ca232e5a93ef69b8bf676ab6d2d02d41
Bazv <brandonv.zamora@gmail.com>
2024-04-18 22:37:43 -0600

    Modulo para mostrar paneles para evitar el error "Infinite circle"

---
+++ logic/open_panels.py
@@ -0,0 +1,27 @@
+"""Modulo de conexión para abrir paneles de la ventana"""
+
+from logic.registro_design import RegisterDesign
+from logic.ctc_design import CtcDesign
+from logic.horario_design import ScheduleDesign
+
+
+class OpenPanel:
+    """Clase que abre los paneles de la ventana."""
+
+    def __init__(self, body):
+        self.body = body
+
+    def show_register_panel(self):
+        """Función que abre panel de registro y limpia los paneles y contenidos de la ventana."""
+
+        RegisterDesign(self.body)
+
+    def show_ctc_panel(self):
+        """Función que abre panel de ctc y limpia los paneles y contenidos de la ventana."""
+
+        CtcDesign(self.body)
+
+    def show_schedule_panel(self):
+        """Función que abre panel de horario y limpia los paneles y contenidos de la ventana."""
+
+        ScheduleDesign(self.body)
