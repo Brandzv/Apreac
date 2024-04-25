477e7767bf5069f4fae9dfce46afd47e9014b2e7
Bazv <brandonv.zamora@gmail.com>
2024-04-25 08:52:09 -0600

    Ajusta para evitar que el texto se salga de las columna nombre y programa

--- logic/bitacora_pdf.py
+++ logic/bitacora_pdf.py
@@ -19,17 +19,6 @@ class BitacoraPDF:
             doc = SimpleDocTemplate(
                 f"BitacoraUso{date_current_time}.pdf", pagesize=landscape(letter))
 
-            # Estilo de la tabla
-            bitacora_style = TableStyle([
-                ('BACKGROUND', (0, 0), (-1, 0), 'gray'),
-                ('TEXTCOLOR', (0, 0), (-1, 0), 'white'),
-                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                ('BACKGROUND', (0, 1), (-1, -1), 'white'),
-                ('GRID', (0, 0), (-1, -1), 1, 'black'),
-            ])
-
             # Estilo del encabezado
             header_style = TableStyle([
                 ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
@@ -40,24 +29,6 @@ class BitacoraPDF:
             title_style.fontSize = 24
             title_style.leftIndent = 170
             title_style.leading = 30
-
-            # Crear la tabla
-            headers = ["No.", "PC", "Fecha", "Nombre de Usuario", "Alumno/Docente",
-                       "Programa", "Hora de Entrada", "Hora de Salida", "Actividad"]
-
-            # Crear la lista de filas
-            data = [headers]
-
-            # Ejecutar consulta para obtener los datos para la tabla
-            bitacora_uso = cursor.execute(
-                "SELECT no, pc, fecha, nombreAlumno, rol, programa, horaEntrada, horaSalida, actividad FROM bitacoraUso")
-
-            # Obtener los datos de la base de datos y agregarlos a la lista de filas
-            for fila in bitacora_uso.fetchall():
-                data.append(fila)
-
-            table = Table(data)
-            table.setStyle(bitacora_style)
 
             # Logo de la UNID
             logo_unid = './resource/LogoUnid.png'
@@ -73,6 +44,50 @@ class BitacoraPDF:
             header_table = Table(header_table_data, colWidths=[110, 570])
 
             header_table.setStyle(header_style)
+
+            # Estilo de la tabla
+            bitacora_style = TableStyle([
+                ('BACKGROUND', (0, 0), (-1, 0), 'gray'),
+                ('TEXTCOLOR', (0, 0), (-1, 0), 'white'),
+                ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+                ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+                ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+                ('BACKGROUND', (0, 1), (-1, -1), 'white'),
+                ('GRID', (0, 0), (-1, -1), 1, 'black'),
+            ])
+
+            # Crear header de la tabla
+            headers = ["No.", "PC", "Fecha", "Nombre de Usuario", "Alumno/Docente",
+                       "Programa", "Hora de Entrada", "Hora de Salida", "Actividad"]
+
+            # Crear la lista de filas
+            data = [headers]
+
+            # Ejecutar consulta para obtener los datos para la tabla
+            bitacora_uso = cursor.execute(
+                "SELECT no, pc, fecha, nombreAlumno, rol, programa, horaEntrada, horaSalida, actividad FROM bitacoraUso")
+
+            # Obtener los datos de la base de datos y agregarlos a la lista de filas
+            for fila in bitacora_uso.fetchall():
+                nombre_usuario = fila[3]
+                programa = fila[5]
+
+                # Dividir el texto en líneas de máximo 30 caracteres (ajusta según tus necesidades)
+                text_nombre_usuario = [nombre_usuario[i:i+30]
+                                       for i in range(0, len(nombre_usuario), 30)]
+                text_programa = [programa[i:i+30]
+                                 for i in range(0, len(programa), 30)]
+
+                # Convertir la tupla a una lista
+                fila_data = list(fila)
+                fila_data[3] = "\n".join(text_nombre_usuario)
+                fila_data[5] = "\n".join(text_programa)
+
+                # Agregar la fila modificada a la lista de datos
+                data.append(fila_data)
+
+            table = Table(data)
+            table.setStyle(bitacora_style)
 
             story.append(header_table)
             story.append(table)
